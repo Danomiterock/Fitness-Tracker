14b98a2b7262bf24a01bc8f410b0b3f8b07e8a5c
Daniel Lucas <danomiterock@yahoo.com>
2021-04-14 09:06:29 -0700

    create function to connect to mongoose. export app and routes

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ const routes = require("./controllers");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
