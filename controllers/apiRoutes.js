51d82adb54ba008bd8431aa63e58742eb6f2d21b
Daniel Lucas <danomiterock@yahoo.com>
2021-04-15 08:34:50 -0700

    add post and get range to apiRoutes

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -19,5 +19,21 @@ router.put('/api/workouts/:id', (req, res) => {
     .catch((err) => console.log(err));
 });
 
-
+router.post("/api/workouts", (req, res) => {
+    const body = req.body;
+    Workout.create(body)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => console.log(err));
+  });
+  
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => console.log(err));
+  });
+  
 module.exports = router;
